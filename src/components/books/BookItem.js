2ce776cdb975f393513575457a0dd2fec5ce8944
Sergey Lunev <lunev.s@gmail.com>
2023-02-10 00:33:47 +0300

    DIG-33 create main menu and add styles

--- src/components/books/BookItem.js
+++ src/components/books/BookItem.js
@@ -1,9 +1,17 @@
 import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTrash} from "@fortawesome/free-solid-svg-icons";
 
 const BookItem = ({ book }) => {
 
     return (
-        <li>{book.title} {book.author}</li>
+        <tr>
+            <td>{book._id}</td>
+            <td>{book.title}</td>
+            <td>{book.author}</td>
+            <td>{book.status}</td>
+            <td><FontAwesomeIcon icon={faTrash} size="xs" /></td>
+        </tr>
     );
 }
 
