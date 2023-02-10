6d28ff0450cf6db95a2506b5417e13dd41b93fb7
Sergey Lunev <lunev.s@gmail.com>
2023-02-10 21:30:54 +0300

    DIG-31 beautify page adding a book

--- src/components/books/BookAdd.js
+++ src/components/books/BookAdd.js
@@ -28,11 +28,19 @@ const BookAdd = () => {
     }
     
     return (
-        <form method='post'>
-            <input type='text' placeholder='Enter book title' value={title} onChange={e => setTitle(e.target.value)} />
-            <input type='text' placeholder='Enter book Author' value={author} onChange={e => setAuthor(e.target.value)} />
-            <button type='submit' onClick={submitHandler}>add book</button>
-        </form>
+        <div className="container mb-5">
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="textId1" className="form-label">Enter book title</label>
+                    <input type="text" className="form-control" id="textId1" value={title} onChange={e => setTitle(e.target.value)} />
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="textId2" className="form-label">Enter book Author</label>
+                    <input type="text" className="form-control" id="textId2" value={author} onChange={e => setAuthor(e.target.value)} />
+                </div>
+                <button type="submit" className="btn btn-primary" onClick={submitHandler}>add book</button>
+            </form>
+        </div>
     );
 }
 
